a220b19737786f55e50badfb363533007a40106b
b8mission <b8mission@gmail.com>
2020-02-04 13:10:08 +0200

    init

--- app/code/Student/PowerModule/Block/PowerBlock.php
+++ app/code/Student/PowerModule/Block/PowerBlock.php
@@ -9,13 +9,14 @@ class PowerBlock extends \Magento\Framework\View\Element\Template
     /**
      * Constructor
      *
-     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param \Magento\Framework\View\Element\Template\Context $context
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         array $data = []
-    ) {
+    )
+    {
         parent::__construct($context, $data);
     }
 
@@ -25,6 +26,33 @@ class PowerBlock extends \Magento\Framework\View\Element\Template
     public function show()
     {
         //Your block code
-        return __('<hr>Hello from PowerModule!');
+        return __('<hr>Hello from PowerBlock.php');
+    }
+
+    const COLORS = [
+        "AliceBlue",
+        "AntiqueWhite",
+        "Aqua",
+        "Aquamarine",
+        "Azure",
+        "Beige",
+        "Bisque",
+        "Black",
+        "BlanchedAlmond",
+        "Fuchsia",
+        "Gainsboro",
+        "GhostWhite",
+        "Gold",
+        "GoldenRod",
+        "LawnGreen",
+        "LemonChiffon",
+        "LightBlue",];
+
+    public function getRandomColor()
+    {
+        $quant = count(self::COLORS);
+        $index = random_int(0,$quant-1);
+
+        return self::COLORS[$index] ?? 'N/A';
     }
 }
---
+++ app/code/Student/PowerModule/Plugin/PowerBlockAfter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Student\PowerModule\Plugin;
+
+class PowerBlockAfter
+{
+
+    public function beforeShow(
+        Student\PowerModule\Block\PowerBlock $subject,
+        $result
+        //$functionParam
+    ) {
+        return 'aSA';
+    }
+}
