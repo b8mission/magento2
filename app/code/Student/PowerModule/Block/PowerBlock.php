5cd96ea9741aea195f16b5ccce3300c020b73197
b8mission <b8mission@gmail.com>
2020-02-06 16:57:44 +0200

    PowerBlock constructor update

--- app/code/Student/PowerModule/Block/PowerBlock.php
+++ app/code/Student/PowerModule/Block/PowerBlock.php
@@ -3,58 +3,64 @@
 
 namespace Student\PowerModule\Block;
 
-class PowerBlock extends \Magento\Framework\View\Element\Template
-{
+use Magento\Catalog\Model\ResourceModel\CategoryFactory;
+use Magento\Catalog\Model\ResourceModel\Collection;
+class PowerBlock extends \Magento\Framework\View\Element\Template {
 
-    /**
-     * Constructor
-     *
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        array $data = []
-    )
-    {
-        parent::__construct($context, $data);
-    }
+  /**
+   * Constructor
+   *
+   * @param \Magento\Framework\View\Element\Template\Context $context
+   * @param array $data
+   */
+  public function __construct(
+    \Magento\Framework\View\Element\Template\Context $context,
+    array $data = [],
+    CategoryFactory $category_factory
+  ) {
+
+    //$all = $category_factory->create(['include_in_menu' => TRUE]);
+    $all = $category_factory->create() ->getCategories(2);
+
+    parent::__construct($context, $data);
+  }
 
 
-    public $text_prop = '[Hello from PowerBlock.php]';
+  public $text_prop = '[Hello from PowerBlock.php]';
+
   /**
    * @return string
    */
-    public function show()
-    {
-        //Your block code
-        return __((string)$this->text_prop);
-    }
+  public function show() {
 
-    const COLORS = [
-        "AliceBlue",
-        "AntiqueWhite",
-        "Aqua",
-        "Aquamarine",
-        "Azure",
-        "Beige",
-        "Bisque",
-        "Black",
-        "BlanchedAlmond",
-        "Fuchsia",
-        "Gainsboro",
-        "GhostWhite",
-        "Gold",
-        "GoldenRod",
-        "LawnGreen",
-        "LemonChiffon",
-        "LightBlue",];
+    return __((string) $this->text_prop);
+  }
 
-    public function getRandomColor()
-    {
-        $quant = count(self::COLORS);
-        $index = random_int(0,$quant-1);
+  const COLORS = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+  ];
 
-        return self::COLORS[$index] ?? 'N/A';
-    }
+  public function getRandomColor() {
+    $quant = count(self::COLORS);
+    $index = random_int(0, $quant - 1);
+
+    return self::COLORS[$index] ?? 'N/A';
+  }
+
 }
