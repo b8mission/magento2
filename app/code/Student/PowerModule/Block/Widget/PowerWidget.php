bfac0c9128e65e4937200cd733a0960cef68950e
b8mission <b8mission@gmail.com>
2020-02-18 19:10:42 +0200

    widget sceleton added

---
+++ app/code/Student/PowerModule/Block/Widget/PowerWidget.php
@@ -0,0 +1,14 @@
+<?php
+namespace Student\PowerModule\Block\Widget;
+
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class PowerWidget extends Template implements BlockInterface
+{
+
+    protected $_template = "widget/powerwidget.phtml";
+
+
+}
---
+++ app/code/Student/PowerModule/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        'slick': 'Student_PowerModule/js/slick.min',
+        'slick_kick': 'js/slick_kick',
+    },
+
+    shim: {
+        deps: ['jquery'],
+    },
+}
