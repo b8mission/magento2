9a4cefe8a9eebdf357ec0e29b66561a19e54c5b0
b8mission <b8mission@gmail.com>
2020-02-25 19:24:19 +0200

    input value bind changed to textInput; a new span with a subscribe-way text-filler

--- app/code/Student/PowerModule/view/frontend/web/js/ko.usage.js
+++ app/code/Student/PowerModule/view/frontend/web/js/ko.usage.js
@@ -5,17 +5,22 @@ define([
     function (Component, ko) {
 
         return Component.extend({
-            inputText: ko.observable(''),
-            span2Text: ko.observable(''),
-            span2IsBold: false,
-            span2Style: ko.observable('normal'),
+            inputText: ko.observable(''),           //same text-bind property for input and span1
+            span2Text: ko.observable(''),           //span2 text-bind property. changes by onclick method
+            span2IsBold: false,                                //bold_or_normal text-style indicator
+            span2Style: ko.observable('normal'),    //span2 style-bind property
+            span3Text: ko.observable(''),           //span3 text-bind property. changes by a subscribe feature
 
             initialize: function (config) {
                 //document.ko = this; //for debug purposes
 
                 this._super(); //parent's constructor
-
                 self = this;
+
+                self.inputText.subscribe(function () {
+
+                    self.span3Text(self.inputText());
+                });
             },
 
             onclick: function () {
