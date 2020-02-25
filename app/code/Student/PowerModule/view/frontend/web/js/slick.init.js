fc5f537fe321ffc2bedac18fd23c38edc2275944
b8mission <b8mission@gmail.com>
2020-02-25 12:16:29 +0200

    slick.init
    slick.init.js lib was rewrited
    now it's defined with 'define' way and requires slides-quantity parameter;
    
    parameter slides-quantity added to js-lib initializer in powerwidget.phtml template;
    slick data-attribute was removed from slider's div tag

--- app/code/Student/PowerModule/view/frontend/web/js/slick.init.js
+++ app/code/Student/PowerModule/view/frontend/web/js/slick.init.js
@@ -1,8 +1,27 @@
-require([
-    'jquery',
-    'slick'
-], function ($) {
-    $(document).ready(function () {
-        $(".slider").slick();
-    });
-});
+define([
+        'uiComponent',
+        'jquery',
+        'slick',
+    ],
+    function (Component, $) {
+
+        return Component.extend({
+            slidesQuantity: 1,
+
+            initialize: function (config) {
+                document.cfg = config;
+
+                let quantity = config.slidesQuantity;
+                if (typeof quantity !== "number")
+                    throw new Error('slick.init: a bad parameter was given.');
+
+                $(".slider").slick({
+                    infinite: true,
+                    slidesToShow: quantity,
+                    slidesToScroll: quantity
+                });
+            },
+
+        });
+    },
+);
