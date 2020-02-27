7c6d60a76cc2be87fa7d87059e8adbaeb4a68dfa
b8mission <b8mission@gmail.com>
2020-02-27 23:48:53 +0200

    new plugin

--- app/code/Student/PowerModule/Plugin/AlsoViewedListProduct.php
+++ app/code/Student/PowerModule/Plugin/AlsoViewedListProduct.php
@@ -2,9 +2,40 @@
 
 namespace Student\PowerModule\Plugin;
 
-class AlsoViewedListProduct{
+use Vovayatsyuk\Alsoviewed\Block\ListProduct;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 
-    public function afterToHtml(){
+class AlsoViewedListProduct
+{
+    private $prod_collection_factory;
 
+    public function __construct(CollectionFactory $prod_collection_factory)
+    {
+        $this->prod_collection_factory = $prod_collection_factory;
+    }
+
+    public function afterToHtml(
+        ListProduct $subject,
+        $result
+    ) {
+
+    }
+
+    public function afterGetLoadedProductCollection(
+        ListProduct $subject,
+        $result
+    ) {
+
+        //exit if not our template is used
+        if ($subject->getTemplate() !== 'Student_PowerModule::widget/also_viewed_power.phtml') {
+            return $result;
+        }
+
+        //$products = $subject->getChildBlock('products');
+        $result = $this->prod_collection_factory
+            ->create()
+            ->addFieldToFilter('entity_id', 0);
+
+        return $result;
     }
 }
